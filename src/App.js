7a03917d8d2130325dd28ef40c2912fcaae4ed63
mehdiviolet <mehdiviolet@yahoo.com>
2024-11-25 12:55:49 +0100

    edit commit

--- src/App.js
+++ src/App.js
@@ -137,7 +137,7 @@ function FormAddFriend({ setFriends }) {
   }
   return (
     <form className="form-add-friend" onSubmit={handleSubmit}>
-      <label>😀 Friendname</label>
+      <label>😀 Friend name</label>
       <input
         type="text"
         value={name}
